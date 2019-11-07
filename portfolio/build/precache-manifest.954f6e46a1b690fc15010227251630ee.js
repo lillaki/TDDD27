908195c9d73971209afa89eb599538c40bbd97a5
Ronja Faltin <ronjafaltin@gmail.com>
2019-11-07 19:11:53 +0100

    test

---
+++ portfolio/build/precache-manifest.954f6e46a1b690fc15010227251630ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebafc56738924df3229ac406ea54bfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dc46cfa5da783f535ed",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5bcb019b2c3ce58a2951",
+    "url": "/static/js/2.bcd42778.chunk.js"
+  },
+  {
+    "revision": "7dc46cfa5da783f535ed",
+    "url": "/static/js/main.93d72db5.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
